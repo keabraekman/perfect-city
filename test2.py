33256d5652a2d26bc1659885640a209349a57a0f
kea <kea.braekman@mail.com>
2021-02-24 22:02:34 -0800

    Added some folders, need to save html to txt. ran into an edge case  in Maryland

--- test2.py
+++ test2.py
@@ -1,11 +1,15 @@
 import sys
 import time
 import requests
+import os
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
+from openpyxl import load_workbook
+import pickle
 
-# workbook = Workbook('perfect-city.xlsx')
-workbook = Workbook()
+import re
+
+workbook = load_workbook('perfect-city.xlsx')
 worksheet = workbook.active
 
 states = ["Alaska", "Alabama", "Arkansas", "Arizona", 
@@ -27,9 +31,11 @@ column_titles = ['City', 'State', 'White', 'Hispanic', 'Black', 'Asian',
 
 def get_html(url):
     re = requests.get(url)
+    print(type(re.text))
     return BeautifulSoup(re.text, 'html.parser')
 
 def get_cities(state):
+    print('\t Adding ', state)
     soup = get_html('https://www.city-data.com/city/' + state + '.html')
     cities = []
     for c in soup.find_all('tr', {'class': 'rB'}):
@@ -56,7 +62,7 @@ def create_city_list():
 def fill_cities_column(cities):
     row = 2
     for c in cities:
-        worksheet.cell(row = row, column = 1, value = c)
+        worksheet.cell(row = row, column = 1, value = c.replace(' ', '-'))
         row += 1
 
 
@@ -71,8 +77,6 @@ def find_first_empty_row(column):
     return r
 
 
-
-
 def fill_state():
     for s in states:
         number_of_rows = number_of_cities(s)
@@ -81,16 +85,86 @@ def fill_state():
             worksheet.cell(row = first_empty_row+i, column = 2, value = s)
         workbook.save('perfect-city.xlsx')
 
-print('fill_headers')
-fill_headers(column_titles)
-print('cities')
-cities = create_city_list()
-print('fill_cities_column')
-fill_cities_column(cities)
-print('fill_state')
-fill_state()
+
+def white(soup, row):
+    return soup.find(text='White alone').parent.parent.find(string=re.compile('%'))
+
+def fill_white():
+    row = 2
+    # if(worksheet.cell(row = row, column = 3).value is not None)
+    while(worksheet.cell(row = row, column = 1).value is not None):
+        row += 1
+        if(worksheet.cell(row = row, column = 3).value is None):
+            city = worksheet.cell(row = row, column = 1).value
+            state = worksheet.cell(row = row, column = 2).value
+            print('ADDING : ' + city + ', ' + state + '\t row = ', row)
+            print(100*row/6849 , '% Done')
+            url = 'https://www.city-data.com/city/' + city + '-' + state + '.html'
+            worksheet.cell(row = row, column = 3).value = white(get_html(url), row)
+            if(row % 10 == 0):
+                print('saved spreadsheet')
+                workbook.save('perfect-city.xlsx')
 
 
-workbook.save('perfect-city.xlsx')
 
 
+# print('fill_headers')
+# fill_headers(column_titles)
+# print('Cities')
+# cities = create_city_list()
+# print('fill_cities_column')
+# fill_cities_column(cities)
+# print('fill_state')
+# fill_state()
+
+# workbook.save('perfect-city.xlsx')
+
+
+
+# DELETE WHITE Column
+# for i in range(1, 10000):
+#     worksheet.cell(row = i, column = 3).value = None
+# workbook.save('perfect-city.xlsx')
+
+
+
+# print('Adding whites')
+# fill_white()
+
+
+
+
+def get_str(url):
+    re = requests.get(url)
+    return re.text
+
+path = '/Users/keabraekman/Documents/Personal/perfect-city'
+
+
+
+# for s in states:
+#     print('creating : ' + path + '/' + s)
+#     os.mkdir(path + '/' + s)
+
+
+row = 2
+while(worksheet.cell(row = row, column = 1).value is not None):
+    city = worksheet.cell(row = row, column = 1).value
+    state = worksheet.cell(row = row, column = 2).value
+    print('creating : ' + path + '/' + state + '/' + city)
+    os.mkdir(path + '/' + state + '/' + city)
+    row += 1
+
+
+
+
+
+
+
+
+# detect the current working directory and print it
+# path = os.getcwd()
+# print ("The current working directory is %s" % path)
+
+
+# workbook.save('perfect-city.xlsx')]
